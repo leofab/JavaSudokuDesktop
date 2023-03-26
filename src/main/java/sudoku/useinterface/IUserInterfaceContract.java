aa46024767452d7d0fdd32d3a47caee6e229829f
leofab <leofabr3@gmail.com>
2023-03-26 16:02:24 -0300

    userInterface package implement

--- src/main/java/sudoku/useinterface/IUserInterfaceContract.java
+++ src/main/java/sudoku/useinterface/IUserInterfaceContract.java
@@ -1,4 +1,18 @@
 package sudoku.useinterface;
 
+import sudoku.domain.Game;
+
 public interface IUserInterfaceContract {
+    interface EventListener {
+        void onSudokuInput(int x, int y, int input);
+        void onDialogClick();
+    }
+
+    interface View {
+        void setListener(IUserInterfaceContract.EventListener listener);
+        void updateSquare(int x, int y, int input);
+        void updateBoard(Game game);
+        void showDialog(String Message);
+        void showError(String message);
+    }
 }
